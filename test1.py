cd2a8dd47a088f72642ff0b54c51f0121c2eefe1
毛 <1296193245@qq.com>
2022-03-21 17:18:19 +0800

    python variable

---
+++ test1.py
@@ -0,0 +1,44 @@
+def print_hi(param):
+    pass
+
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
+    pi = 3.1415926
+    print(pi)
+    str1 = "hello world"
+    print(str1)
+    sum1 = 100 + 20
+    print(sum1)
+    sum1 = sum1 * 6
+    print(sum1)
+
+    n = 74
+    print(n)
+    print(type(n))
+    # 给x赋值一个很大的整数
+    x = 8888888888888888888888
+    print(x)
+    print(type(x))
+    # 给y赋值一个很小的整数
+    y = -7777777777777777777777
+    print(y)
+    print(type(y))
+
+    # 十六进制
+    hex1 = 0x45
+    hex2 = 0x4Af
+    print("hex1Value: ", hex1)
+    print("hex2Value: ", hex2)
+    # 二进制
+    bin1 = 0b101
+    print('bin1Value: ', bin1)
+    bin2 = 0B110
+    print('bin2Value: ', bin2)
+    # 八进制
+    oct1 = 0o26
+    print('oct1Value: ', oct1)
+    oct2 = 0O41
+    print('oct2Value: ', oct2)
+
+    input()
